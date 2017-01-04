0aa2a336245e0743f8fdaa71d36a4c1fcf7e6691
Donnie Ellstrom <dsvene@gmail.com>
2017-01-03 18:06:49 -0800

    Setup filesystem, create database, setup put route to database

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var path = require('path');
+var Hashids = require("hashids");
+var hashids = new Hashids("this is a salty hashbrown");
+
 var app = express();
+var db = require("./models");
+
+app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(ejsLayouts)
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
+
+app.post('/links', function(req, res){
+  console.log(req.body);
+  db.link.create(req.body).then(function(link){
+    res.redirect('/links/' + link.id);
+  });
+});
+
+app.get('/links/:id', function(req, res){
+
+})
 
 var server = app.listen(process.env.PORT || 3000);
 
